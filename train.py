ff97a5b799cf54ed80887231cd9ab303b1c049aa
Fatih HAŞLAK <62775596+Fatih-Haslak@users.noreply.github.com>
2023-12-19 23:04:49 +0300

    toggle optimizer problem fixed

--- train.py
+++ train.py
@@ -12,7 +12,7 @@ class ConvNextModel(L.LightningModule):
     def __init__(
         self,
         num_classes: int=6,
-        lr: float = 0.0001,
+        lr: float = 0.001,
         b1: float = 0.9,
         b2: float = 0.999,
     ):
@@ -23,6 +23,7 @@ class ConvNextModel(L.LightningModule):
         model_conv=ConvNext(num_classes) # model
         self.convnext=model_conv.model()
         self.criterion = nn.CrossEntropyLoss() # loss function
+        #self.optimizer_model= self.optimizers()
 
     def forward(self, z):
         return self.convnext(z) #torch.image generator forward
@@ -30,24 +31,29 @@ class ConvNextModel(L.LightningModule):
     def training_step(self, batch,batch_idx):
         #image, #label
         img, label = batch
+       
         #label = label.view(-1, 1).float()
-        optimizer_model= self.optimizers()
-        self.toggle_optimizer(optimizer_model)
+        self.optimizer_model= self.optimizers()
+        
+        #self.toggle_optimizer(optimizer_model)
+        
         self.predicted_label = self(img) #img
         #calculate loss
+        
         loss = self.criterion(self.predicted_label, label)
-        self.log("Loss", loss, prog_bar=True) #loggla
+        
+        self.log("train_Loss", loss, prog_bar=True) #loggla
+        
         self.manual_backward(loss,retain_graph=True)# backward
-        optimizer_model.step()
-        optimizer_model.zero_grad()
-        self.untoggle_optimizer(optimizer_model)
+        self.optimizer_model.step()
+        self.optimizer_model.zero_grad()
+        #self.untoggle_optimizer(optimizer_model)
 
     def validation_step(self, batch, batch_idx):
         x, y = batch
         #y = y.view(-1, 1).float()
         #x=self.validation_z.type_as(x)
         predict = self.convnext(x)
-        
         loss = self.criterion(predict,y)
         self.log('val_loss', loss,on_epoch=True,prog_bar=True)
 
@@ -58,12 +64,12 @@ class ConvNextModel(L.LightningModule):
 
         optimizer_model = torch.optim.Adam(self.convnext.parameters(), lr=lr, betas=(b1, b2))
     
-        return [optimizer_model], []
+        return optimizer_model
     
 
 if __name__ == "__main__":
-    data_path=r"C:\Users\90546\Desktop\multı_clas\seg_train"
-    batch_size=4
+    data_path="/home/fatih/Desktop/convnext/seg_train"
+    batch_size=16
     data = Dataloader(data_path,batch_size)
     model=ConvNextModel()    
     train,val=data.load_data() #load data
@@ -71,8 +77,7 @@ if __name__ == "__main__":
     trainer = L.Trainer(
     accelerator="auto",
     devices=1,
-    max_epochs=40,
-    benchmark=True,
+    max_epochs=150,
     #strategy="ddp_find_unused_parameters_true",
     #num_nodes=1
     )
