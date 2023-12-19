00f09352103ac65603551b79701139a69e916603
Fatih HAŞLAK <62775596+Fatih-Haslak@users.noreply.github.com>
2023-12-19 23:05:23 +0300

    remove softmax and add more linear connection

--- model/convnext.py
+++ model/convnext.py
@@ -9,7 +9,7 @@ class ConvNext():
     def __init__(self,num_classes):
         #pre_trained modeli yükle
         #models.convnext_tiny(pre_trained=True)
-        self.ConvNext = models.convnext_tiny(weights="ConvNeXt_Tiny_Weights.DEFAULT",pretrained=True)
+        self.ConvNext = models.convnext_tiny(weights="IMAGENET1K_V1",pretrained=True)
         self.num_classes=num_classes
         # ConvNext modelinin son katmanlarını değiştir
         self.num_ftrs = self.ConvNext.classifier[2].in_features  # Son fully connected katmanın giriş özellik sayısı
@@ -21,12 +21,18 @@ class ConvNext():
         new_fc_layers = nn.Sequential(
             nn.Linear(self.num_ftrs, 512),
             nn.ReLU(),
-            nn.Dropout(0.5),
+            nn.LayerNorm(512),
             nn.Linear(512, 256),
             nn.ReLU(),
-            nn.Dropout(0.5),
-            nn.Linear(256, self.num_classes),
-            nn.Softmax(dim=1)  
+            nn.LayerNorm(256),
+            nn.Linear(256, 128),
+            nn.ReLU(),
+            nn.LayerNorm(128),
+            nn.Linear(128, 64),
+            nn.ReLU(),
+            nn.LayerNorm(64),
+            nn.Linear(64,self.num_classes),
+            #nn.Softmax(dim=1)  
         ) 
 
         self.ConvNext.classifier[2]=new_fc_layers #
@@ -39,6 +45,7 @@ class ConvNext():
         # Yeni eklenen katmanın parametrelerini güncelleme / Modelin son katmanını egıtıcez
         for param in self.ConvNext.classifier.parameters():
             param.requires_grad = True
+         
         
 
         new_model=self.ConvNext
@@ -46,9 +53,12 @@ class ConvNext():
         return new_model
 
 
+# conv=ConvNext(6)
+# model=conv.model()
 
-"""
-conv=ConvNext(6)
-model=conv.model()
-print(model)
-"""
+
+# print(model.classifier)
+
+# for param in model.classifier.parameters():
+# 	print(param)
+
